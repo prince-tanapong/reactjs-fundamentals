75aa390923cb84d183c17e393f72d0b3b3ac4c05
Tanapong Udomrattanasirichai <tanapong@MacBook-Pro.hkw.proteus-tech.com>
2016-12-01 14:59:48 +0700

    Create routes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 var React = require("react");
 var ReactDom = require("react-dom");
+var routes = require('./config/routes')
 
 var USER_DATA = {
   name: "tanapong",
   username: "prince-tanapong",
   imageUrl: "https://avatars3.githubusercontent.com/u/8855366?v=3&s=460"
 }
-
 
 var Hello = React.createClass({
   render: function(){
@@ -22,13 +22,26 @@ var ProfilePic = React.createClass({
   }
 })
 
+var Link = React.createClass({
+  changeUrl: function(){
+    window.location.replace( this.props.href )
+  },
+  render:function(){
+    return (
+      <span style={{color: "blue", cursor: "pointer"}} onClick={ this.changeUrl }>
+        { this.props.children }
+      </span>
+    )
+  }
+})
+
 var ProfileLink = React.createClass({
   render:function(){
     return(
       <div>
-        <a href={ "https://www.github.com/" + this.props.username}>
+        <Link href={ "https://www.github.com/" + this.props.username}>
           { this.props.username }
-        </a>
+        </Link>
       </div>
       )
   }
@@ -56,7 +69,7 @@ var Avatar = React.createClass({
   }
 })
 
-ReactDom.render(<Avatar user={USER_DATA} />, document.getElementById('app'))
+ReactDom.render(routes, document.getElementById('app'))
 
 
 
